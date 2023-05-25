fd177bf7d3006769d12884e5d90b17c030b1c6f2
Raymond <raymondpattend@icloud.com>
2023-05-25 11:12:42 -0400

    First commit

---
+++ src/main/java/com/reflexian/levitycosmetics/LevityCosmetics.java
@@ -0,0 +1,17 @@
+package com.reflexian.levitycosmetics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LevityCosmetics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
