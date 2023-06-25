3cc6597adb458c0070847322c54d49fc6bbed39b
Raymond <raymondpattend@icloud.com>
2023-06-25 17:04:20 -0400

    Added everything basically, just minor bugs to fix.32

--- src/main/java/com/reflexian/levitycosmetics/data/objects/user/UserData.java
+++ src/main/java/com/reflexian/levitycosmetics/data/objects/user/UserData.java
@@ -16,7 +16,6 @@ import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import wtf.sysdm.levitycredits.LevityCreditsSpigot;
 
 import java.io.Serializable;
 import java.sql.ResultSet;
